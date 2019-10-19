92451d4a0797e175d7d97d4efdd03bb3a299af43
Rico Mitchell <ricomitch@icloud.com>
2019-10-19 04:50:21 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,9 @@ def my_collect(array)
   i = 0
   new_collection = [ ]
   while i < array.length
-  collection >> yield (array[i])
+  new_collection << yield (array[i])
   i = i + 1
   end
+  new_collection
 end
 
